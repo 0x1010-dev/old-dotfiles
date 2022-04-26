4e07e78c1323216b37ad9b815d65caf375038d29
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2022-04-26 10:56:47 -0700

    Add minimap.

--- configs/astronvim/lua/user/plugins/init.lua
+++ configs/astronvim/lua/user/plugins/init.lua
@@ -11,5 +11,17 @@ return {
         config = function()
             require("nightfox").setup(require "user.plugins.nightfox")
         end
+    },
+    {
+        "wfxr/minimap.vim",
+        -- cmd = {
+        --     "Minimap",
+        --     "MinimapClose",
+        --     "MinimapToggle"
+        -- },
+        setup = function()
+            vim.g.minimap_auto_start = 1
+            vim.g.minimap_auto_start_win_enter = 1
+        end
     }
 }
