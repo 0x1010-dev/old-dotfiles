69124ca75e7945dfe17e130e62dadcec355d2814
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2022-04-27 15:43:31 -0700

    config updates

--- configs/astronvim/lua/user/plugins/init.lua
+++ configs/astronvim/lua/user/plugins/init.lua
@@ -14,14 +14,24 @@ return {
     },
     {
         "wfxr/minimap.vim",
-        -- cmd = {
-        --     "Minimap",
-        --     "MinimapClose",
-        --     "MinimapToggle"
-        -- },
         setup = function()
             vim.g.minimap_auto_start = 1
             vim.g.minimap_auto_start_win_enter = 1
+
+            vim.api.nvim_create_autocmd('WinEnter', {
+                pattern = "*",
+                callback = function()
+                    local mmwinnr = vim.fn.bufwinnr("-MINIMAP-")
+
+                    if mmwinnr == -1 then
+                        return
+                    end
+
+                    if vim.fn.winnr() == mmwinnr then
+                        vim.api.nvim_command("wincmd t")
+                    end
+                end
+            })
         end
     }
 }
