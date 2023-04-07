8572493b29de5c6d4df255e1f8c3f1ad4ab3b2f6
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2023-04-06 19:44:26 -0700

    Update AstroNvim

--- configs/astronvim/lua/user/plugins/init.lua
+++ configs/astronvim/lua/user/plugins/init.lua
@@ -1,6 +1,20 @@
 return {
     {
-      "lambdalisue/suda.vim",
-      cmd = { "SudaWrite", "SudaRead" }
+        "lambdalisue/suda.vim",
+        cmd = { "SudaWrite", "SudaRead" }
+    },
+    {
+        "goolord/alpha-nvim",
+        opts = function(_, opts)
+            opts.section.header.val = {
+                [[                                  __]],
+                [[     ___     ___    ___   __  __ /\_\    ___ ___]],
+                [[    / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\]],
+                [[   /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \]],
+                [[   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+                [[    \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+            }
+            opts.config.layout[3].val = 2
+        end,
     }
 }
